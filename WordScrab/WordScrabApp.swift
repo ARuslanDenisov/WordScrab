d2ed175a137adfaf156a67103cd855a251d52d8f
ARuslanDenisov <angess47@gmail.com>
2024-06-02 01:16:04 +0700

    Initial Commit

---
+++ WordScrab/WordScrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrabApp.swift
+//  WordScrab
+//
+//  Created by Руслан on 02.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
